4227224b2da54d4f95f78bc26c194e090204d6f6
Joe <joerity@gmail.com>
2022-07-03 09:24:15 +0100

    Add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,37 @@
 import Image from "next/image";
 import headerImage from "../public/header.png";
-import styles from "../styles/Home.module.css";
 import SharedHead from "../components/sharedHead";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="p-8 mx-auto text-center">
       <SharedHead />
 
-      <main className={styles.main}>
-        <Image data-testid="header-image" src={headerImage} alt="Inexplicable Games Logo"></Image>
-        <p data-testid="intro-text-primary" className={styles.introTextPrimary}>
+      <main>
+        <Image
+          data-testid="header-image"
+          src={headerImage}
+          alt="Inexplicable Games Logo"
+        ></Image>
+        <p className="my-10 text-2xl" data-testid="intro-text-primary">
           A one man games company based in the UK, working with talented
           freelancers from around the world.
         </p>
-        <p data-testid="intro-text-secondary" className={styles.introTextQuote}>
+        <p
+          className="max-w-md mx-auto text-xl"
+          data-testid="intro-text-secondary"
+        >
           <q>
             Two roads diverged in a wood, and I ... I took the one just covered
             with brambles, Got covered in cuts and barely made it out alive.
-          </q> ~ Robert Frost
+          </q>{" "}
+          ~ Robert Frost
         </p>
       </main>
 
-      <footer data-testid="footer" className={styles.footer}></footer>
+      <footer className="border-t-2 border-black mt-10 fixed bottom-0" data-testid="footer">
+        © Inexplicable Games
+      </footer>
     </div>
   );
 }
