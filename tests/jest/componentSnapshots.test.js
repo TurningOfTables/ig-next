6352c09a7b8970464510a7959c93b92c2377d5e4
Joe <joerity@gmail.com>
2022-07-09 14:16:02 +0100

    Update tests

--- tests/jest/componentSnapshots.test.js
+++ tests/jest/componentSnapshots.test.js
@@ -2,14 +2,26 @@ import { render } from "@testing-library/react";
 import GameCards from "../../components/gameCards";
 import SharedHead from "../../components/sharedHead";
 import Footer from "../../components/footer";
+import FeatureSection from "../../components/featureSection";
 
 it("renders <SharedHead /> component unchanged", () => {
   const { container } = render(<SharedHead />);
   expect(container).toMatchSnapshot();
 });
 
-it("renders <GameCardsContainer /> component unchanged", () => {
+it("renders <GameCards /> component unchanged", () => {
   const { container } = render(<GameCards />);
+  expect(container).toMatchSnapshot();
+});
+
+it("renders <FeatureSection /> component unchanged", () => {
+  const { container } = render(
+    <FeatureSection
+      title="Test Title"
+      text="Test Text"
+      imgSrc="/tmlCard-1.png"
+    />
+  );
   expect(container).toMatchSnapshot();
 });
 
