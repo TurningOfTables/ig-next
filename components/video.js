4bd9f979b427141f1985806a5e00e0fc57e9bb32
Joe <joerity@gmail.com>
2022-07-27 12:06:57 +0100

    Slightly increase size of video component

--- components/video.js
+++ components/video.js
@@ -1,6 +1,6 @@
 export default function Video({ videoId, title }) {
   return (
-    <div className="max-w-lg mt-10 mb-20 mx-auto">
+    <div className="max-w-2xl mt-10 mb-20 mx-auto">
       <h2 className="text-2xl mb-5">Trailer</h2>
       <div className="relative pb-[56.25%] h-0 overflow-hidden">
         <iframe
